90b1b11af649404ef8eb5ca6e649dae95e9e31f8
Tala Hethnawi <hethnawi.tala@gmail.com>
2021-11-08 20:20:33 +0200

    Adding book and topic models

---
+++ models/__init__.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+from utils import engine
+
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    in_stock_count = Column(Integer, default=0, nullable=False)
+    price = Column(Float, default=0.0, nullable=False)
+    topic_id = Column(Integer, ForeignKey('topic.id'))
+    topic = relationship('Topic', backref='books')
+
+
+class Topic(Base):
+    __tablename__ = 'topic'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+
+Base.metadata.create_all(engine)
